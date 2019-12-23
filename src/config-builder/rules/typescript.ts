1daf0577fb74ee82f3ee0dec2c2b1607c3169e0a
Dennis Miasoutov <denis.miasoutov@mocoding.com>
2019-12-23 01:57:08 +0000

    reformat

--- src/config-builder/rules/typescript.ts
+++ src/config-builder/rules/typescript.ts
@@ -1,5 +1,8 @@
 import * as webpack from "webpack";
 export const typescript: webpack.Rule = {
   test: /\.(ts|tsx)?$/,
-  use: { loader: "ts-loader", options: {onlyCompileBundledFiles: true, allowTsInNodeModules: true} },
+  use: {
+    loader: "ts-loader",
+    options: { onlyCompileBundledFiles: true, allowTsInNodeModules: true },
+  },
 };
