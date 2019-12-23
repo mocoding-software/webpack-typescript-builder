555977788b177c88833bc0b893c261ba12aee38d
Dennis Miasoutov <denis.miasoutov@mocoding.com>
2019-12-22 19:27:59 -0500

    Enabled cli. Removed exclude from typescript rule.

--- src/config-builder/rules/tslint.ts
+++ src/config-builder/rules/tslint.ts
@@ -1,6 +1,6 @@
 import webpack from "webpack";
 export const tslint: webpack.Rule = {
-  exclude: [/node_modules/],
+  include: __dirname,
   test: /\.(ts|tsx)?$/,
   use: "tslint-loader",
 };
--- src/config-builder/rules/typescript.ts
+++ src/config-builder/rules/typescript.ts
@@ -1,6 +1,6 @@
 import * as webpack from "webpack";
 export const typescript: webpack.Rule = {
-  exclude: [/node_modules/],
+  include: __dirname,
   test: /\.(ts|tsx)?$/,
   use: { loader: "ts-loader" },
 };
